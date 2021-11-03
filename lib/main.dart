369d0a30ffb480e589d6838ffbafb51c7dd5a05a
akkarapol <killua71042@gmail.com>
2021-11-03 21:52:43 +0700

    Update

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:mobileappweek2/config/constant.dart';
+import 'package:mobileappweek2/screen/dashboard.dart';
+import 'package:mobileappweek2/screen/login.dart';
+//import 'package:mobileappweek2/screen/packageimage.dart';
+//import 'package:mobileappweek2/screen/packagelocation.dart';
+//import 'package:mobileappweek2/screen/packagevideo.dart';
+import 'package:mobileappweek2/screen/register.dart';
+import 'screen/home.dart';
+import 'screen/index.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Index(),
+      routes: {
+        'Login': (context) => Login(),
+        'Register': (context) => Register(),
+        'Dashboard': (context) => Dashboard(),
+        //'Image': (context) => PackageImage(),
+        //'Video': (context) => PackageVideo(),
+        //'Location': (context) => PackageLocation(),
+      },
+      theme: ThemeData(
+        primaryColor: pColor,
+        secondaryHeaderColor: sColor,
+      ),
+    );
+  }
+}
